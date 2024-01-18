d8614420db10238447efbbb61f8d69183ef7b72c
Vinícius Hoyer <contact@vhoyer.dev>
2024-01-18 15:16:18 -0300

    :arrow_up: Upgrade nuxt and add bit of info for Vuex

--- plugins/plugin.js
+++ plugins/plugin.js
@@ -1,5 +1,14 @@
 import { log } from '~/log.js'
+import { createStore } from 'vuex';
 
-export default function () {
+log('create plugin');
+
+const store = createStore({});
+
+// but it is a MutableReactiveHandlerProxy,
+// must be a Vuex thing
+log(`Vuex state: [isRef=${isRef(store.state)}]`, store.state);
+
+export default defineNuxtPlugin(function () {
   log('plugin')
-}
+});
